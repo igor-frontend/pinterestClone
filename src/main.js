d643d3beccd0d0f373669be9d4e3507c19cf0fe2
IgorDev <igorcamponotus@gmail.com>
2026-08-20 17:33:11 +0200

    cambio de funcionalidad de boton inicio

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import createHeader from "./components/Header/Header.js";
 import createGrid from "./components/Grid/Grid.js";
 
 const app = document.querySelector("#app");
+let cacheInicial = null;
 
+const mostrarInicio = async () => {
+    await createGrid();
+    const main = app.querySelector(".grid-container");
+
+    if (main) {
+        cacheInicial = main.cloneNode(true);
+    }
+};
 const buscar = async (query) => {
     const title = app.querySelector(".discover-title");
     if (title) {
@@ -11,18 +20,26 @@ const buscar = async (query) => {
     await createGrid(query);
 };
 
-const volverAlInicio = async () => {
+const volverAlInicio = () => {
     const title = app.querySelector(".discover-title");
     if (title) {
         title.textContent = "Discover images";
     }
-
     const input = app.querySelector(".search-input");
     if (input) {
         input.value = "";
     }
-    await createGrid();
+    const mainActual = app.querySelector(".grid-container");
+    if (mainActual) {
+        mainActual.remove(); // Lo destruimos por completo
+    }
+    if (cacheInicial) {
+        app.appendChild(cacheInicial.cloneNode(true));
+    } else {
+        mostrarInicio();
+    }
 };
+
 
 const header = createHeader(buscar, volverAlInicio);
 app.appendChild(header);
@@ -32,4 +49,4 @@ title.className = "discover-title";
 title.textContent = "Discover images";
 app.appendChild(title);
 
-volverAlInicio();
+mostrarInicio();
